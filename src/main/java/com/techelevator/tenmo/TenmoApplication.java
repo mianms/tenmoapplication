470fade608d6e77d55cedde4c57c11bd953d2c2c
Maryam Mian <mianmaryam96@gmail.com>
2023-08-23 12:33:53 -0500

    hello

--- src/main/java/com/techelevator/tenmo/TenmoApplication.java
+++ src/main/java/com/techelevator/tenmo/TenmoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TenmoApplication {
-
+    //hello
     public static void main(String[] args) {
         SpringApplication.run(TenmoApplication.class, args);
     }

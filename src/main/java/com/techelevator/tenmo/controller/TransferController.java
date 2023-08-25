714934c70347ddf7d158431e46f46dfbdf9bae6d
Priya Stephen <priyastephenv@gmail.com>
2023-08-25 02:16:11 -0600

    more transfer checks

--- src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -4,6 +4,7 @@ import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.service.TransferServiceDao;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,25 +22,34 @@ public class TransferController {
 
     private TransferDao transferDao;
     private AccountDao accountDao;
-    public TransferController(TransferDao transferDao, AccountDao accountDao1) {
+    private TransferServiceDao transferServiceDao;
+    public TransferController(TransferDao transferDao, AccountDao accountDao1, TransferServiceDao transferServiceDao1) {
         this.transferDao = transferDao;
         this.accountDao = accountDao1;
+        this.transferServiceDao = transferServiceDao1;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "/transfer", method = RequestMethod.POST)
     public Transfer addTransfer(@Valid @RequestBody Transfer transfer) {
 
-        Transfer createdTransfer = transferDao.createTransfer(transfer);
+        int accountID = transfer.getFromAccountId();
+        BigDecimal balance = accountDao.getAccountById(accountID).getBalance();
+        BigDecimal amountToTransfer = transfer.getTransferAmount();
+        Transfer createdTransfer = new Transfer();
 
-        //update balances
-        BigDecimal transferAmount = createdTransfer.getTransferAmount();
-        int fromId = createdTransfer.getFromAccountId();
-        int toId =createdTransfer.getToAccountId();
+      if  ( transferServiceDao.amountGreaterThanZero(amountToTransfer) && transferServiceDao.amountLessThanBalance(amountToTransfer,balance)) {
+           createdTransfer = transferDao.createTransfer(transfer);
 
-        accountDao.updateFromAccount(transferAmount, fromId);
+          //update balances
+          BigDecimal transferAmount = createdTransfer.getTransferAmount();
+          int fromId = createdTransfer.getFromAccountId();
+          int toId = createdTransfer.getToAccountId();
 
-        accountDao.updateToAccount(transferAmount, toId);
+          accountDao.updateFromAccount(transferAmount, fromId);
+
+          accountDao.updateToAccount(transferAmount, toId);
+      }
 
 
         return createdTransfer;
